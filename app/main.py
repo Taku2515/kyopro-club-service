5318758a43a1a6fb19ccf809aa41e9ca621ced43
Taku2515 <it.program.2357@gmail.com>
2025-01-16 22:31:06 +0900

    JSONを返す機能の追加

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from utils import create_json
 
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/fetch")
+def fetch_root():
+    return create_json.get_contests_json()
