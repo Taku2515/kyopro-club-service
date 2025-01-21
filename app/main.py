590c064a84f2927b9239225e378d5dc61e9acaa8
Taku2515 <it.program.2357@gmail.com>
2025-01-21 18:39:42 +0900

    テストケースの作成

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from utils import create_json
+from utils import test_date
 
 
 app = FastAPI()
@@ -9,5 +10,21 @@ def read_root():
     return {"Hello": "World"}
 
 @app.get("/atcoder/upcoming_contests")
-def fetch_root():
+def fetch_contests():
     return create_json.get_contests_json()
+
+@app.get("/atcoder/test1")
+def fetch_test1():
+    return test_date.fetch_not_start_time_json(test_date.not_start_time_json)
+
+@app.get("/atcoder/test2")
+def fetch_test1():
+    return test_date.fetch_not_start_time_json(test_date.contest_type_json)
+
+@app.get("/atcoder/test3")
+def fetch_test1():
+    return test_date.fetch_not_start_time_json(test_date.date_format_json)
+
+@app.get("/atcoder/test4")
+def fetch_test1():
+    return test_date.fetch_not_start_time_json(test_date.date_cornercase)
---
+++ app/utils/test_date.py
@@ -0,0 +1,192 @@
+import json
+
+
+def fetch_not_start_time_json(data):
+    return json.loads(data)
+
+not_start_time_json = [
+  {
+    "starttime": "2025-01-25 21:00:00+0900",
+    "title": "AtCoder Beginner Contest 390",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ABC"
+  },
+  {
+    "starttime": "2025-01-26 21:00:00+0900",
+    "title": "AtCoder Regular Contest 191 (Div. 2)",
+    "time": "02:00",
+    "rated_target": "1200 ~ 2399",
+    "category": "ARC"
+  },
+  {
+    "starttime": "2025-02-01 21:00:00+0900",
+    "title": "AtCoder Beginner Contest 391",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ABC"
+  },
+  {
+    "starttime": "2025-02-02 19:00:00+0900",
+    "title": "AtCoder Heuristic Contest 042",
+    "time": "04:00",
+    "rated_target": "All",
+    "category": "AHC"
+  },
+  {
+    "starttime": "2025-02-08 21:00:00+0900",
+    "title": "日本レジストリサービス（JPRS）プログラミングコンテスト2025#1（AtCoder Beginner Contest 392）",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ABC"
+  }
+]
+
+
+contest_type_json = [
+  {
+    "starttime": "2025-01-25 21:00:00+0900",
+    "title": "AtCoder Beginner Contest 390",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ABC"
+  },
+  {
+    "starttime": "2025-01-26 21:00:00+0900",
+    "title": "AtCoder Regular Contest 191 (Div. 2)",
+    "time": "02:00",
+    "rated_target": "1200 ~ 2399",
+    "category": "ARC"
+  },
+  {
+    "starttime": "2025-02-01 21:00:00+0900",
+    "title": "AtCoder Regular Contest 191 (Div. 1)",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ARC"
+  },
+  {
+    "starttime": "2025-02-02 19:00:00+0900",
+    "title": "AtCoder Heuristic Contest 042",
+    "time": "04:00",
+    "rated_target": "All",
+    "category": "AHC"
+  },
+  {
+    "starttime": "2025-02-08 21:00:00+0900",
+    "title": "AtCoder Grand Contest 098",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "AGC"
+  }, 
+  {
+    "starttime": "2025-02-08 21:00:00+0900",
+    "title": "Normal Contest",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": ""
+  }
+]
+
+
+date_format_json = [
+  {
+    "starttime": "2025-01-25 21:00:00+0900",
+    "title": "AtCoder Beginner Contest 390",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ABC"
+  },
+  {
+    "starttime": "2025/01/26 21:00:00+0900",
+    "title": "AtCoder Regular Contest 191 (Div. 2)",
+    "time": "02:00",
+    "rated_target": "1200 ~ 2399",
+    "category": "ARC"
+  },
+  {
+    "starttime": "2025-02 21:00:00+0900",
+    "title": "AtCoder Regular Contest 191 (Div. 1)",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ARC"
+  },
+  {
+    "starttime": "2025-02-02 19/00/00+0900",
+    "title": "AtCoder Heuristic Contest 042",
+    "time": "04:00",
+    "rated_target": "All",
+    "category": "AHC"
+  },
+  {
+    "starttime": "2025-02-08 21:00:00+0900",
+    "title": "AtCoder Grand Contest 098",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "AGC"
+  }, 
+  {
+    "starttime": "2025-02-08 21:00:00+0900",
+    "title": "Normal Contest",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": ""
+  }
+]
+
+date_cornercase = [
+  {
+    "starttime": "2025-01-25 21:00:00+0900",
+    "title": "AtCoder Beginner Contest 390",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ABC"
+  },
+  {
+    "starttime": "2025-01-25 21:00:00+0900",
+    "title": "AtCoder Regular Contest 191 (Div. 2)",
+    "time": "02:00",
+    "rated_target": "1200 ~ 2399",
+    "category": "ARC"
+  },
+  {
+    "starttime": "2025-01-31 21:00:00+0900",
+    "title": "AtCoder Regular Contest 191 (Div. 2)",
+    "time": "02:00",
+    "rated_target": "1200 ~ 2399",
+    "category": "ARC"
+  },
+  {
+    "starttime": "2025-02-01 21:00:00+0900",
+    "title": "AtCoder Beginner Contest 391",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ABC"
+  },
+  {
+    "starttime": "2025-02-02 10:00:00+0900",
+    "title": "AtCoder Heuristic Contest 042",
+    "time": "04:00",
+    "rated_target": "All",
+    "category": "AHC"
+  },
+  {
+    "starttime": "2025-02-02 19:00:00+0900",
+    "title": "AtCoder Heuristic Contest 042",
+    "time": "04:00",
+    "rated_target": "All",
+    "category": "AHC"
+  },
+  {
+    "starttime": "2025-02-08 10:00:00+0900",
+    "title": "日本レジストリサービス（JPRS）プログラミングコンテスト2025#1（AtCoder Beginner Contest 392）",
+    "time": "01:40",
+    "rated_target": "~ 1999",
+    "category": "ABC"
+  }
+  ]
+
+not_start_time_json = json.dumps(not_start_time_json, ensure_ascii=False, indent=2)
+contest_type_json = json.dumps(contest_type_json, ensure_ascii=False, indent=2)
+date_format_json = json.dumps(date_format_json, ensure_ascii=False, indent=2)
+date_cornercase = json.dumps(date_cornercase, ensure_ascii=False, indent=2)
